bd41f2124cc5c54bb5d1a5072334e9eda84c5307
apple <rocker_pg@163.com>
2018-05-25 09:43:32 +0800

    init

---
+++ src/main/java/cn/rocker/Utils/NonNullCheck.java
@@ -0,0 +1,17 @@
+package cn.rocker.Utils;
+
+import org.junit.Test;
+
+/**
+ * @author rocker
+ * @version V1.0
+ * @Description:
+ * @date 2018/5/25 9:40
+ */
+public class NonNullCheck {
+    @Test
+    public void fun1(){
+        String str = null;
+        System.out.println(str == null);
+    }
+}

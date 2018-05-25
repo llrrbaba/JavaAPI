2e8ed28f5405cad66d96472c78d326c475755cec
apple <rocker_pg@163.com>
2018-05-25 09:42:29 +0800

    init

--- src/main/java/cn/rocker/collection/list/SortList.java
+++ src/main/java/cn/rocker/collection/list/SortList.java
@@ -1,5 +1,11 @@
 package cn.rocker.collection.list;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author rocker
  * @version V1.0
@@ -7,5 +13,25 @@ package cn.rocker.collection.list;
  * @date 2018/5/23 17:53
  */
 public class SortList {
+    @Test
+    public void sort(){
+        List<String> list = new ArrayList();
+        list.add("a");
+        list.add("r");
+        list.add("g");
+        list.add("j");
+        list.add("q");
+        list.add("b");
+        for(String str: list){
+            System.out.print(str);
+        }
 
+        System.out.println();
+
+        Collections.sort(list);
+        for(String str: list){
+            System.out.print(str);
+        }
+
+    }
 }

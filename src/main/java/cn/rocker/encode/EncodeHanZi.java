8361aa8873341d87dc114a3fa3173597f6fcce91
apple <rocker_pg@163.com>
2018-06-27 21:37:47 +0800

    汉字的编解码，URL中有用到

---
+++ src/main/java/cn/rocker/encode/EncodeHanZi.java
@@ -0,0 +1,29 @@
+package cn.rocker.encode;
+
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+/**
+ * @author rocker
+ * @version V1.0
+ * @Description:    将汉字编码和解码
+ *                    在传递给url的参数中有的汉字可能需要编码，可以用此类中的方法
+ * @date 2018/6/27 21:25
+ */
+public class EncodeHanZi {
+    @Test
+    public void test() throws UnsupportedEncodingException {
+        String text = "京a123456";
+
+        //将汉字编码
+        text = URLEncoder.encode(text,"utf-8");
+        System.out.println(new String(text));//%E4%BA%AC
+
+        //将编码后的汉字解码
+        text = URLDecoder.decode(text,"utf-8");
+        System.out.println(text);//京
+    }
+}

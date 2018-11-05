344202e003b2d205d59ae212035a8c12f2c9252d
llrrbaba <rocker_pg@163.com>
2018-11-05 18:15:31 +0800

    Lock接口测试

---
+++ src/main/java/cn/rocker/concurrent/lock/LockTest.java
@@ -0,0 +1,50 @@
+package cn.rocker.concurrent.lock;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Lock接口测试
+ * 声明lock要在成员变量声明，如果在方法中声明，则每个线程会获取不同的锁，
+ * b线程不用等待a线程执行unlock()，就可以调用lock()
+ */
+public class LockTest {
+
+    private List<Integer> arrayList = new ArrayList<Integer>();
+    private Lock lock = new ReentrantLock();    //注意这个地方
+
+
+    public static void main(String[] args)  {
+        final LockTest test = new LockTest();
+
+        new Thread(){
+            public void run() {
+                test.insert(Thread.currentThread());
+            }
+        }.start();
+
+        new Thread(){
+            public void run() {
+                test.insert(Thread.currentThread());
+            }
+        }.start();
+    }
+
+
+    public void insert(Thread thread) {
+        lock.lock();
+        try {
+            System.out.println(thread.getName()+"得到了锁");
+            for(int i=0;i<5;i++) {
+                arrayList.add(i);
+            }
+        } catch (Exception e) {
+        }finally {
+            System.out.println(thread.getName()+"释放了锁");
+            lock.unlock();
+        }
+    }
+
+}

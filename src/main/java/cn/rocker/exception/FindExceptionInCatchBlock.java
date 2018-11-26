d9b8139ad4cc1065f1549ee20da4774ccf375b23
llrrbaba <rocker_pg@163.com>
2018-11-26 12:41:45 +0800

    异常测试

--- src/main/java/cn/rocker/exception/FindExceptionInCatchBlock.java
+++ src/main/java/cn/rocker/exception/FindExceptionInCatchBlock.java
@@ -3,23 +3,29 @@ package cn.rocker.exception;
 import org.junit.Test;
 
 /**
+ * 异常的捕捉顺序是？一个catch里嵌套另一个catch，如果抛出异常，堆栈信息的先后顺序是？
+ * 答案是：先抛出内层异常，再抛出外层异常
+ *
  * @author rocker
  * @version V1.0
- * @Description:    异常的捕捉顺序是？一个catch里嵌套另一个catch，如果抛出异常，堆栈信息的先后顺序是？
  * @date 2018/7/9 18:12
  */
 public class FindExceptionInCatchBlock {
 
     @Test
-    public void testFindExceptionInCatchBlock(){
-        System.out.println(getString());
+    public void testFindExceptionInCatchBlock1() {
+        System.out.println(getString1());
     }
 
+    @Test
+    public void testFindExceptionInCatchBlock2() throws Exception {
+        System.out.println(getString2());
+    }
 
-    public String getString(){
+    public String getString1() {
         String result = null;
         try {
-            new String(result.getBytes(),"utf-8");
+            new String(result.getBytes(), "utf-8");
         } catch (Exception e) {
             try {
                 int i = 1 / 0;
@@ -28,8 +34,16 @@ public class FindExceptionInCatchBlock {
             }
             e.printStackTrace();
         }
-
         return "test";
     }
+
+
+    public String getString2() throws Exception{
+        String result = null;
+        new String(result.getBytes(), "utf-8");
+        int i = 1 / 0;
+        return "test";
+    }
+
 
 }
